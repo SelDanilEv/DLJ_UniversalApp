c4776c948069bb2302727ee9f9ff862766a83b20
SelDanilEv <47692381+SelDanilEv@users.noreply.github.com>
2021-01-26 23:26:43 +0300

    init: create project

---
+++ DLJ_UniversalApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DLJ_UniversalApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
